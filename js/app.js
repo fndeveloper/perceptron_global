fd7288e7cdcbd23f6c9c354a308e1c8ff97f4bb6
fndeveloper <fasihnasir60@gmail.com>
2025-07-11 21:28:55 +0500

    c

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 // ===================================================
 document.addEventListener("scroll", () => {
-    const scrollTop = window.scrollY;
-    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-  
-    const scrollPercent = (scrollTop / docHeight) * 100;
-  
-    const topBtn = document.querySelector(".top_btn_1");
-  
-    if (scrollPercent >= 100) {
-      topBtn.innerHTML = `<i class="fa-solid fa-arrow-up"></i>`; // Font Awesome icon
-    } else {
-      topBtn.innerHTML = `${scrollPercent.toFixed(0)}`;
+  const scrollTop = window.scrollY;
+
+console.log(scrollTop);
+
+  const navbar = document.getElementById("navbar");
+  const topBtn = document.querySelector(".top_btn_1");
+
+  if (scrollTop >= 590) {
+
+    if (navbar) {
+      navbar.classList.add("navfix");
+      console.log("e");
+      
     }
-  });
-  
+  } else {
+    
+    if (navbar) {
+      console.log("if");
+
+      navbar.classList.remove("navfix");
+    }
+  }
+});
+
 // ===================================================
 
 // ===================== HEADER CODE START ======================
